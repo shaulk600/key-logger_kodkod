42c3bcef456f9cfe396acd459f601300aa9aaff1
Shaul Kimel <shaullk600@gmail.com>
2025-02-12 14:08:16 +0200

    Update key_logger_collective_shaul.py

--- key_logger_collective_shaul.py
+++ key_logger_collective_shaul.py
@@ -1,14 +1,43 @@
-class KeyLoggerService:
+from abc import ABC, abstractmethod
+from typing import List
+class IKeyLogger(ABC):
+ @abstractmethod
+ def start_logging(self) -> None:
+ pass
+
+ @abstractmethod
+ def stop_logging(self) -> None:
+ pass
+
+ @abstractmethod
+ def get_logged_keys(self) -> List[str]:
+ pass
+
+#2
+
+# iwriter.py
+from abc import ABC, abstractmethod
+class IWriter(ABC):
+@abstractmethod
+def send_data(self, data: str, machine_name: str) -> None:
+pass
+
+#3
+class Encryptor:
     pass
 
+
+#4
 class KeyLoggerManager:
     pass
 
-class FileWriter:
-    pass
-
+#5
 class NetworkWriter:
-    pass
 
-class Encryptor:
-    pass
+   #תוצאה נדרשת
+#send_data(data: str, machine_name: str) -> None
+
+   pass
+
+#חלק 2 בניית Backend
+
